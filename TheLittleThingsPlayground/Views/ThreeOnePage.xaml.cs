16bedc1b1c023423596ac3de0e5aa227660a5410
David Ortinau <david.ortinau@microsoft.com>
2018-07-11 11:03:37 -0500

    initial commit

---
+++ TheLittleThingsPlayground/Views/ThreeOnePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TheLittleThingsPlayground.Views
+{
+    public partial class ThreeOnePage : ContentPage
+    {
+        public ThreeOnePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
