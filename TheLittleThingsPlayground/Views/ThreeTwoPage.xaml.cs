b15107cbb724a76bcd5026b40c9cfd26a623f6b3
David Ortinau <david.ortinau@microsoft.com>
2018-07-19 11:28:48 -0500

    implemented top enhancement highlights from 3.2.0

---
+++ TheLittleThingsPlayground/Views/ThreeTwoPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TheLittleThingsPlayground.Views
+{
+    public partial class ThreeTwoPage : ContentPage
+    {
+        public ThreeTwoPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            FirstEntry.CursorPosition = 4;
+            FirstEntry.SelectionLength = 11;
+        }
+    }
+}
