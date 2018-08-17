c89fe9ba31033390ed91a1da8bb78ecfba217730
David Ortinau <david.ortinau@microsoft.com>
2018-08-17 12:27:14 -0500

    added gesture on span

--- TheLittleThingsPlayground.Android/MainActivity.cs
+++ TheLittleThingsPlayground.Android/MainActivity.cs
@@ -19,8 +19,17 @@ namespace TheLittleThingsPlayground.Droid
 
             base.OnCreate(bundle);
 
+            Xamarin.Essentials.Platform.Init(this, bundle);
+
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
--- TheLittleThingsPlayground/Views/ThreeTwoPage.xaml.cs
+++ TheLittleThingsPlayground/Views/ThreeTwoPage.xaml.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace TheLittleThingsPlayground.Views
 {
     public partial class ThreeTwoPage : ContentPage
     {
+        public string Url
+        {
+            get;
+            private set;
+        } = "xamarin.com";
+
+        public Command TapCommand
+        {
+            get;
+            private set;
+        }
+
         public ThreeTwoPage()
         {
             InitializeComponent();
+
+            BindingContext = this;
+
+            TapCommand = new Command<string>(HandleAction);
         }
 
         protected override void OnAppearing()
@@ -19,5 +35,11 @@ namespace TheLittleThingsPlayground.Views
             FirstEntry.CursorPosition = 4;
             FirstEntry.SelectionLength = 11;
         }
+
+        async void HandleAction(string url)
+        {
+            await Browser.OpenAsync(url);
+        }
+
     }
 }
